da0f0465d999125f5b7d98e358e32f527d1b636c
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-07-18 19:41:18 +0300

    update properties instead of ${version}

--- microservice1/src/main/java/com/example/microservice1/PomUpdater.java
+++ microservice1/src/main/java/com/example/microservice1/PomUpdater.java
@@ -3,6 +3,8 @@ package com.example.microservice1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 import com.example.microservice1.Dependency;
 
@@ -27,6 +30,20 @@ public class PomUpdater {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(pomFile);
 
+             // Parse properties
+            Map<String, String> propertiesMap = new HashMap<>();
+            NodeList propertiesNodes = document.getElementsByTagName("properties");
+            if (propertiesNodes.getLength() > 0) {
+                NodeList propertiesList = propertiesNodes.item(0).getChildNodes();
+                for (int i = 0; i < propertiesList.getLength(); i++) {
+                    Node node = propertiesList.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        propertiesMap.put(node.getNodeName(), node.getTextContent());
+                    }
+                }
+            }
+
+            // Update dependencies
             NodeList dependencies = document.getElementsByTagName("dependency");
             for (int i = 0; i < dependencies.getLength(); i++) {
                 Element element = (Element) dependencies.item(i);
@@ -34,9 +51,27 @@ public class PomUpdater {
                 String artifactId = element.getElementsByTagName("artifactId").item(0).getTextContent();
 
                 if (dependency.getGroupId().equals(groupId) && dependency.getArtifactId().equals(artifactId)) {
-                    if(element.getElementsByTagName("version").item(0) != null){
-                        element.getElementsByTagName("version").item(0).setTextContent(newVersion);
-                        break;
+                    Node versionNode = element.getElementsByTagName("version").item(0);
+                    if (versionNode != null) {
+                        String version = versionNode.getTextContent();
+                        if (version.startsWith("${") && version.endsWith("}")) {
+                            // Update the property value
+                            String propertyName = version.substring(2, version.length() - 1);
+                            if (propertiesMap.containsKey(propertyName)) {
+                                propertiesMap.put(propertyName, newVersion);
+                                // Update the properties in the document
+                                NodeList propertiesList = propertiesNodes.item(0).getChildNodes();
+                                for (int j = 0; j < propertiesList.getLength(); j++) {
+                                    Node node = propertiesList.item(j);
+                                    if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals(propertyName)) {
+                                        node.setTextContent(newVersion);
+                                    }
+                                }
+                            }
+                        } else {
+                            // Update the version directly
+                            versionNode.setTextContent(newVersion);
+                        }
                     }
                 }
             }
